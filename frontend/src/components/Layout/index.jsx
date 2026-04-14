7227277c74ac29f8de3bc2aea8fb4fe8b8af5d35
Ramy Boussad <Mohand-Said-Ram.Boussad@etu.univ-littoral.fr>
2026-04-14 11:50:54 +0200

    pages centrées + placeholder de jeux

--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,19 +1,26 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, Outlet } from "react-router";
+import "./index.css"
 
 export default function Layout(){
+    
     return (
-        <>
-            <ul>
-                <li><Link to="/">Acceuil</Link></li>
-                <li><Link to="/login">Se connecter</Link></li>
-                <li><Link to="/NewUser">Créer un compte</Link></li>
-            </ul>
-            <Outlet/>
+        <div id="page">
+            <header id="menu">
+                <Link to="/"><button id="acceuil">Accueil</button></Link>
+                <div>
+                    <Link to="/login"><button>Se connecter</button></Link>
+                    <Link to="/NewUser"><button>Créer un compte</button></Link>
+                </div>
+                
+            </header>
+            <div id="Outlet">
+                <Outlet/>
+            </div>
             <footer>
                 <span>MyGameList ® 2026</span>
             </footer>
 
-        </>
+        </div>
     )
 }
--- frontend/src/routes/Login/index.jsx
+++ frontend/src/routes/Login/index.jsx
@@ -1,28 +1,48 @@
 import { Link } from "react-router";
+import { useContext, useState } from "react";
+import "./index.css"
 
 export default function Login(){
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (e) => {  /* TODO: surveiller les inputs*/ 
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    function handleSubmit(e) { /* TODO: gèrer le formulaire*/ 
+    e.preventDefault();
+    alert("submited");
+    }
+
     return (
-        <div>
-            <h1>Se Connecter</h1>
-            <div>
-                <strong>
-                    Nom d'Utilisateur
-                </strong>
-                <input type="text"/>
-
-                <strong>
-                    Mot de Passe
-                </strong>
-                <input type="text"/>
-
-                <button>Valider</button>
+        <div id="page">
+            <h1 id="title">Se Connecter</h1>
+            <form id="login" onSubmit={handleSubmit} >
+                <input 
+                    type="text" 
+                    name="pseudo"
+                    placeholder="Pseudo"
+                    value={inputs.pseudo} 
+                    onChange={handleChange}
+                />
+                
+                <input 
+                    type="password" 
+                    name="password"
+                    placeholder="Mot de passe"
+                    value={inputs.password} 
+                    onChange={handleChange}
+                />
+                <input id="submit" type="submit" value="Envoyer" />
+            </form>
+            <div id="alternative">
+                <i>Pas encore de compte?</i>&nbsp;<Link to="/NewUser">Crée en un</Link>
             </div>
 
-            <div>
-                <i>Pas encore de compte?</i> <Link to="/newUser">Crée en un maintenant</Link>
-
-            </div>
-            
         </div>
-    )
+    
+  )
+    
 }
