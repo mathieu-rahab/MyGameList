3fa2aabdb7f907578d1d15fccd4cd59681c61242
rahab.mathieu <mathieu.rahab@protonmail.com>
2026-04-21 19:16:47 +0200

    backend, ajout de Business

---
+++ backend/Mygamelist/Mygamelist.Business/UserService.cs
@@ -0,0 +1,55 @@
+﻿using Mygamelist.Core.Business;
+using Mygamelist.Core.Repository;
+using Mygamelist.Entity;
+
+
+namespace Mygamelist.Business
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User Add(User user)
+        {
+            return _userRepository.Insert(user);
+        }
+
+        public bool Remove(int id)
+        {
+            return _userRepository.Delete(id);
+        }
+
+        public IEnumerable<User> RetrieveAll()
+        {
+            
+            return (_userRepository.SelectAll()
+                .ToList());
+        }
+
+        public User? RetrieveById(int id)
+        {
+            return _userRepository.SelectById(id);
+        }
+
+        public User Update(int id, User user)
+        {
+            return _userRepository.Update(id, user);
+        }
+        
+        public bool EmailExists(string email)
+        {
+            return _userRepository.EmailExists(email);
+        }
+
+        public bool PseudoExists(string pseudo)
+        {
+            return _userRepository.PseudoExists(pseudo);
+        }
+
+    }
+}
---
+++ backend/Mygamelist/Mygamelist.Core/Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Mygamelist.Entity;
+
+namespace Mygamelist.Core.Repository
+{
+    public interface IUserRepository
+    {
+        bool Delete(int id);
+        User Insert(User user);
+        public IEnumerable<User> SelectAll();
+        User? SelectById(int id);
+        User Update(int id, User user);
+        bool EmailExists(string email);
+        bool PseudoExists(string pseudo);
+    }
+}
