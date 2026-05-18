1614066a936a7a6d74337ded03932b9dec4c380c
boussad.ramy <mohand-said-ram.boussad@etu.univ-littoral.fr>
2026-05-18 13:39:11 +0200

    feat(login): implement login form submission with cookies and error handling
    - Added functionality to handle user login, including field validation and form submission to the backend API.
    - Implemented JWT authentication using `react-cookie`.
    - Enhanced error handling for backend and HTTP/network issues.
    - Integrated navigation upon successful login and fallback to previous email state.
    - Updated root component to include `CookiesProvider` for handling cookies.
    - Installed `react-cookie` dependency.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
+import { CookiesProvider } from 'react-cookie';
 import App from './App.jsx'
 import './global.css'
 import '@tabler/icons-webfont/dist/tabler-icons.css';
@@ -7,8 +8,10 @@ import './utils/i18n.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Suspense fallback={<div>Chargement des traductions...</div>}>
-      <App />
-    </Suspense>
+    <CookiesProvider>
+      <Suspense fallback={<div>Chargement des traductions...</div>}>
+        <App />
+      </Suspense>
+    </CookiesProvider>
   </StrictMode>,
 )
