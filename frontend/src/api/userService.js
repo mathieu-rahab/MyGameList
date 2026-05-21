ac7d0432790b486e16ff34b7c77262a112dfa434
rahab.mathieu <mathieu.rahab@protonmail.com>
2026-05-21 18:22:32 +0200

    feat(dashboard): display recent achievements and localization updates
    - Integrated `getRecentAchievements` API in the dashboard.
    - Enhanced game and achievement data handling with customizable language options.
    - Improved UI to include achievements rarity tiers.
    - Adjusted styles for better layout consistency.

--- frontend/src/api/userService.js
+++ frontend/src/api/userService.js
@@ -4,8 +4,13 @@ export const useUserService = () => {
     const apiCall = useApiCall();
 
     return {
-        getRecentGames: async (userId, count = 4, includeAchievements = true) => {
-            return apiCall(`/User/${userId}/recent-games/?count=${count}&includeAchievements=${includeAchievements}`,{method:'GET'});
+        getRecentGames: async (userId, count = 4, includeAchievements = false, l = "french") => {
+            return apiCall(`/User/${userId}/recent-games/?count=${count}&includeProgression=${includeAchievements}&l=${l}`,{method:'GET'});
+        },
+
+        getRecentAchievements: async (userId, count = 4, withPercent, l = "french") => {
+            return apiCall(`/User/${userId}/recent-achievements/?count=${count}&includeRarity=${withPercent}&l=${l}`,{method:'GET'});
+
         }
     };
 };
