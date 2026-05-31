bf63cd4bcb0957e6d234bd4c34147d00118ef89f
rahab.mathieu <mathieu.rahab@protonmail.com>
2026-05-31 10:02:13 +0200

    feat(steam): update search and game info DTOs to use consistent ID naming and add image properties

--- backend/Mygamelist/Mygamelist.Contracts/DTOs/Steam/SearchGameDto.cs
+++ backend/Mygamelist/Mygamelist.Contracts/DTOs/Steam/SearchGameDto.cs
@@ -2,7 +2,8 @@ namespace Mygamelist.Contracts.DTOs.Steam;
 
 public class SearchGameDto
 {
-    public required int AppId { get; set; }
+    public required int Id { get; set; }
     public required string Name { get; set; }
     public required string TinyImage { get; set; }
+    public required string Image { get; set; }
 }
